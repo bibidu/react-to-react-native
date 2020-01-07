826a02c7ff7cd6d09b81a767bf842a3315a67f0e
duxianzhang <787753295@qq.com>
2020-01-07 17:15:04 +0800

    feat clean阶段移除uniqueId

--- packages/core/asts/package/visitors/clean.js
+++ packages/core/asts/package/visitors/clean.js
@@ -1,5 +1,11 @@
 module.exports = function clean({ ctx, t }) {
 
   return {
+    JSXElement(path) {
+      const uniqueIdPrefix = ctx.getUniqueIdPrefix()
+      const uniqueIdAttr = ctx.jsxUtils.getJSXAttribute(path, uniqueIdPrefix)
+      // 移除uniqueId
+      uniqueIdAttr.remove()
+    }
   }
 }
