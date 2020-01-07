ab6bcde9e489fc8a5891d7cea8fa647ab68a084f
duxianzhang <787753295@qq.com>
2020-01-07 18:19:48 +0800

    feat 重构convertReactTagToRN

--- packages/core/asts/processAST/visitors/convertReactTagToRN.js
+++ packages/core/asts/processAST/visitors/convertReactTagToRN.js
@@ -1,59 +1,61 @@
+// 是否是用户自定义组件
+const isComponent = (tagName) => tagName.charAt(0) !== tagName.charAt(0).toLowerCase()
+
+// 收集使用到的React Native组件
+function collectRNUsingComponentName(ctx, tagName) {
+  ctx.addUsingRNComponentName(tagName)
+}
+
+// 替换标签名
+function replaceTagName(path, t, newTagName) {
+  const newTagPath = t.JSXIdentifier(newTagName)
+  path.get('openingElement').get('name').replaceWith(newTagPath)
+  path.get('closingElement').get('name').replaceWith(newTagPath)
+}
+
+// 保存标签自带样式
+function saveStyleFromTagName(path, ctx, styles) {
+  if (Object.keys(styles).length) {
+    const uniqueId = ctx.jsxUtils.getJSXAttributeValue(path, ctx.enums.UNIQUE_ID)
+    if (!uniqueId || !uniqueId.node || !uniqueId.node.value) {
+      ctx.error('不存在uniqueId')
+    }
+    ctx.addUniqueTagStyle(uniqueId.node.value, styles)
+  }
+}
+
 module.exports = function convertReactTagToRN({ ctx, t }) {
-  function replaceAndSaveTagStyle(path) {
-    const tagNamePath = path.get('openingElement').get('name')
-    const { tag, styles, isComponent } = getRNTagMappingFromHtml(tagNamePath.node.name)
-
-    if (!isComponent) {
-      // 收集使用到的React Native组件
-      ctx.addUsingRNComponentName(tag)
-
-      // 替换标签
-      const newTagPath = t.JSXIdentifier(tag)
-      tagNamePath.replaceWith(newTagPath)
-      path.get('closingElement').get('name').replaceWith(newTagPath)
   
-      // 保存标签自带样式
-      if (Object.keys(styles).length) {
-        const uniqueId = ctx.jsxUtils.getJSXAttributeValue(path, ctx.enums.UNIQUE_ID)
-        if (!uniqueId || !uniqueId.node || !uniqueId.node.value) {
-          ctx.error('不存在uniqueId')
+  function replacement(path) {
+    const tagName = path.get('openingElement').get('name').node.name
+    if (isComponent(tagName)) return
+
+    function _replacement() {
+      switch (tagName) {
+        case 'div': {
+          return {
+            tag: 'View'
+          }
         }
-        ctx.addUniqueTagStyle(uniqueId.node.value, styles)
+
+        default: {
+          return {
+            tag: 'View'
+          }
+        }
       }
     }
+    const { tag, styles = {} } = _replacement()
+
+    collectRNUsingComponentName(ctx, tag)
+    replaceTagName(path, t, tag)
+    saveStyleFromTagName(path, ctx, styles)
   }
 
   return {
     JSXElement(path) {
-      replaceAndSaveTagStyle(path)
+      replacement(path)
     }
   }
 }
 
-function getRNTagMappingFromHtml(tagName) {
-  const mapping = {
-    tag: '',
-    styles: {},
-    // 是否是自定义组件
-    isComponent: tagName.charAt(0) !== tagName.charAt(0).toLowerCase(),
-  }
-  if (mapping.isComponent) return mapping
-
-  switch(tagName) {
-    case 'div': {
-      mapping.tag = 'View'
-      break
-    }
-
-    case 'span': {
-      mapping.tag = 'Text'
-      break
-    }
-
-    default: {
-      mapping.tag = 'View'
-    }
-  }
-  
-  return mapping
-}
