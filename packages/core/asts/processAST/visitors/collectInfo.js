06b6e378840fbbf4e8c673d6adab647ab7438239
duxianzhang <787753295@qq.com>
2020-01-07 14:18:38 +0800

    feat 修改visitors.collectInfo方法名、添加收集export组件名的注释

--- packages/core/asts/processAST/visitors/collectInfo.js
+++ packages/core/asts/processAST/visitors/collectInfo.js
@@ -1,7 +1,8 @@
-module.exports = function addUniqueId({ ctx, t }) {
+module.exports = function collectInfo({ ctx, t }) {
   return {
     ExportDefaultDeclaration(path) {
       const { name } = path.get('declaration').node
+      // 收集导出的组件名
       ctx.collections.exports.push(name)
     }
   }
