7c29f222e361936099ee8e685e9756a5c5623a40
duxianzhang <787753295@qq.com>
2020-01-07 16:41:17 +0800

    feat add cssToReactNative模板

--- packages/core/utils/cssToReactNative.js
+++ packages/core/utils/cssToReactNative.js
@@ -1,6 +1,20 @@
-module.exports = function cssToReactNative() {
-  return new Promise((resolve, reject) => {
-    this.log('cssToReactNative')
-    resolve()
-  })
+const transform = require('css-to-react-native').default
+
+module.exports = function cssToReactNative(obj) {
+  try {
+    const arrayStyle = objStyleToTransformArray(obj)
+    const result = transform(arrayStyle)
+    return result
+  } catch (error) {
+    return '[ERROR] cssToReactNative error'
+  }
+}
+
+const objStyleToTransformArray = (obj) => {
+  const array = []
+  for (let key of Object.keys(obj)) {
+    const value = obj[key]
+    array.push([key, value])
+  }
+  return array
 }
