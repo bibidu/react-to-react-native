fe133dc1dfb124a2ac65a43bf49008502128ec82
duxianzhang <787753295@qq.com>
2020-01-11 18:03:41 +0800

    style 修改package -> packageAST,解决保留字冲突

---
+++ packages/core/asts/packageAST/index.js
@@ -0,0 +1,10 @@
+const traverse = require('@babel/traverse').default
+const t = require('@babel/types')
+const visitors = require('./visitors')
+
+module.exports = function packageAST(ast) {
+  this.log('package')
+
+  visitors.forEach(visitor => traverse(ast, visitor({ ctx: this, t, })))
+  return ast
+}
