7e7dd1828ce2c27fce0e4fbcf3a3b7306b8df735
787753295@qq.com <787753295@qq.com>
2020-01-14 23:07:52 +0800

    fix 合并非继承样式和继承样式的顺序错误

--- packages/core/utils/mixinInheritAndOther.js
+++ packages/core/utils/mixinInheritAndOther.js
@@ -1,5 +1,5 @@
 const merge = require('./mergeByKey')
 
 module.exports = function mixinInheritAndOther(mixinedStyleExceptInherit, inheritStyle) {
-  return merge(mixinedStyleExceptInherit, inheritStyle)
+  return merge(inheritStyle, mixinedStyleExceptInherit)
 }
