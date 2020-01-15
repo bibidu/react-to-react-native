a9746d85c276ac187be37f9706f436cc361eaa2d
duxianzhang <787753295@qq.com>
2020-01-15 10:48:30 +0800

    feat 编译结果添加banner

--- packages/core/utils/generateReactNativeComponent.js
+++ packages/core/utils/generateReactNativeComponent.js
@@ -11,17 +11,26 @@ module.exports = function generateReactNativeComponent() {
     return `const ${this.enums.STYLESHEET_NAME} = StyleSheet.create(` + JSON.stringify(finalStyleObject, null, 2) + ')'
   }
 
+  function genTopBanner() {
+    return `//
+// Created by node lib \`react-to-react-native\`.
+// 
+`
+  }
+
   const {
     usingRNComponentNames,
     finalStyleObject,
     collections,
   } = this
 
+  const topBanner = genTopBanner()
   const importReact = this.astUtils.ast2code(collections.importReactPath)
   const usingCode = genUsingComponentCode(usingRNComponentNames)
   const styleSheet = genStyleSheet.call(this, finalStyleObject)
   const component = this.astUtils.ast2code(this.afterPackageCode)
   const result = [
+    topBanner,
     importReact,
     usingCode,
     component,
