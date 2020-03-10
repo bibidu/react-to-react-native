52d268cc79de51b46821764840d5dac6a26d297e
787753295@qq.com <787753295@qq.com>
2020-03-10 17:11:16 +0800

    feat [web-server]express增加跨域支持

--- packages/web-server/app.js
+++ packages/web-server/app.js
@@ -7,12 +7,12 @@ const bodyParser = require('body-parser')
 /* static */
 const PORT = 3000
 
-// app.all('*',function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*'); //当允许携带cookies此处的白名单不能写’*’
-//   res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
-//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
-//   next();
-// });
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //当允许携带cookies此处的白名单不能写’*’
+  res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.urlencoded({ extended: true }))
