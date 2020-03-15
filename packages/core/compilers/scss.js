86e20ad9699fc8868785e92ac742408ce75e6242
787753295@qq.com <787753295@qq.com>
2020-03-15 20:18:01 +0800

    feat [core] scss编译阶段收集错误、未输入scss代码直接结束scss编译

--- packages/core/compilers/scss.js
+++ packages/core/compilers/scss.js
@@ -3,15 +3,22 @@ const sass = require('node-sass')
 module.exports = function scss(code, filePath) {
   return new Promise((resolve, reject) => {
     this.log('scssCompiler')
+    console.log(code);
+    if (!code.trim()) {
+      return resolve('')
+    }
     sass.render({
       data: code,
       file: filePath || undefined
     }, (err, r) => {
       if (err) {
-        return this.error(err)
+        this.errors.add(err && err.message)
+        this.error(err)
+        resolve('')
+      } else {
+        const css = r.css.toString()
+        resolve(css)
       }
-      const css = r.css.toString()
-      resolve(css)
     })
   })
 }
