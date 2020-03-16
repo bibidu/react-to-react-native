10d7b73c92007ad7495464eebcaecc2545abe407
787753295@qq.com <787753295@qq.com>
2020-03-16 15:27:37 +0800

    fix [web-server] 遗漏了部署后js文件中的localhost替换任务

--- packages/web-server/tasks.js
+++ packages/web-server/tasks.js
@@ -3,13 +3,16 @@ const os = require('os')
 const publicIp = require('public-ip')
 const { execSync } = require('child_process')
 
-const ENTRY = './static/index.html'
+const ENTRYS = [
+  './static/index.html',
+  './static/js/index.js',
+]
 
 const html = fs.readFileSync(ENTRY, 'utf8')
 
 publicIp.v4().then(ip => {
   console.log(`ip ${ip}`)
-  return  fs.writeFileSync(ENTRY, html.replace(/localhost/g, ip), 'utf8')
+  return ENTRYS.forEach(ENTRY => fs.writeFileSync(ENTRY, html.replace(/localhost/g, ip), 'utf8'))
 }).then(() => {
   console.log(execSync(`
     echo "installing web-server dependencies" &&
