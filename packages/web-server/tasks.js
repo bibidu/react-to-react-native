30c219bcf39bc7dab548cce38a986f7bdc6df2e4
787753295@qq.com <787753295@qq.com>
2020-03-16 15:33:30 +0800

    fix [web-server] 替换localhost时 文件内容读取错误

--- packages/web-server/tasks.js
+++ packages/web-server/tasks.js
@@ -8,11 +8,13 @@ const ENTRYS = [
   './static/js/index.js',
 ]
 
-const html = fs.readFileSync(ENTRY, 'utf8')
 
 publicIp.v4().then(ip => {
   console.log(`ip ${ip}`)
-  return ENTRYS.forEach(ENTRY => fs.writeFileSync(ENTRY, html.replace(/localhost/g, ip), 'utf8'))
+  return ENTRYS.forEach(ENTRY => {
+    const content = fs.readFileSync(ENTRY, 'utf8')
+    fs.writeFileSync(ENTRY, content.replace(/localhost/g, ip), 'utf8')
+  })
 }).then(() => {
   console.log(execSync(`
     echo "installing web-server dependencies" &&
