0b30a6b74e6722844ff98a0f4a0c3e37b922663b
787753295@qq.com <787753295@qq.com>
2020-04-18 14:36:51 +0800

    style: [self-components] 修改组件命名

--- packages/self-components/src/App.tsx
+++ packages/self-components/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Button from './components/Button/preview'
 // import Calendar from './components/Calendar/preview'
-// import Modal from './components/Modal/preview'
+import Modal from './components/Modal/preview'
 // import Share from './components/Share/preview'
 // import PopupComment from './components/PopupComment/preview'
 // import SearchBar from './components/SearchBar/preview'
-import Drag from './components/Drag/preview'
+// import Drag from './components/Drag/preview'
 
 function App() {
-  return <Drag />
+  return <Modal />
 }
 
 export default App
--- packages/self-components/src/components/Modal/preview.tsx
+++ packages/self-components/src/components/Modal/preview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '../Button'
-import Pop from '.'
+import Modal from '.'
 import './preview.scss'
 
 
@@ -9,35 +9,37 @@ class App1 extends React.Component {
     show: false
   }
 
-  togglePop = () => {
+  toggleModal = () => {
     this.setState({
       show: !this.state.show
     })
   }
 
-  renderPop = () => {
-    return this.state.show ? <Pop
+  renderModal = () => {
+    return this.state.show ? <Modal
       visible={true}
       type="center"
       title={'title'}
       picSrc={"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2018939532,1617516463&fm=26&gp=0.jpg"}
       textList={[`我是很长的内容很长的内容`]}
-      confirmCBK={this.togglePop}
-      cancelCBK={this.togglePop}
-      clickPicCBK={this.togglePop}
+      confirmCBK={this.toggleModal}
+      cancelCBK={this.toggleModal}
+      clickPicCBK={this.toggleModal}
     /> : null
   }
+
   render() {
     return (
-      <div rn-scroll="true" className="preview-container">
-        <h2>Pop</h2>
-        <div className="title">基本类型</div>
-        <div className="playground">
-          <Button type="primary" onClick={this.togglePop}>显示Pop弹窗</Button>
+      <React.Fragment>
+        <div rn-scroll="true" className="preview-container">
+          <h2>Modal</h2>
+          <div className="title">基本类型</div>
+          <div className="playground">
+            <Button type="primary" onClick={this.toggleModal}>显示Modal弹窗</Button>
+          </div>
         </div>
-
-        {this.renderPop()}
-      </div>
+        {this.renderModal()}
+      </React.Fragment>
     )
   }
 }
